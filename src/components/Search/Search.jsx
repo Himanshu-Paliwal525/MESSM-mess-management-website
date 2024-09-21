74e004f0537d929ac5fe5af8a8edebee1064caf1
Himanshu Paliwal <paliwalhimanshu525@gmail.com>
2024-09-21 19:10:43 +0530

    search component update

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -10,7 +10,7 @@ const Search = () => {
                 {" "}
                 Here are All the Search Results
             </h1>
-            <div className="grid grid-cols-4">
+            <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2">
                 {searchedItems.map((mess, index) => {
                     const id = mess._id;
                     mess = mess._source;
